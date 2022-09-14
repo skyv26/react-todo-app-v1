f773f808c829e2fd89b428c617a1100db2959f24
skyv26 <dev.aakashv@gmail.com>
2022-09-14 23:34:20 +0530

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Header from './components/Header/Header';
+import TodoInput from './components/TodoInput/TodoInput';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <TodoInput />
+    </>
   );
 }
 
