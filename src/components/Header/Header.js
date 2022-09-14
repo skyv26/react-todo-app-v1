7bd879443b34c976a632b6011e53551989c012d3
skyv26 <dev.aakashv@gmail.com>
2022-09-14 22:47:23 +0530

    Update `Header` Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import style from './Header.module.css';
 class Header extends React.PureComponent {
   render() {
     return (
-      <header>
+      <header className={style.Header}>
         <a href="https://" className={style.Logo}>todos</a>
       </header>
     );
