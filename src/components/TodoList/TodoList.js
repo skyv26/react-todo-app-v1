ea156d9f187656d341fc79780984a8256420e92c
skyv26 <dev.aakashv@gmail.com>
2022-09-15 22:18:29 +0530

    Implement `edit-todo` basic functionality

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { IoMdTrash } from 'react-icons/io';
 import style from './TodoList.module.css';
 
-class TodoList extends React.PureComponent {
+class TodoList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -23,23 +23,37 @@ class TodoList extends React.PureComponent {
           completed: false,
         },
       ],
+      // updateTodoToggle: false,
     };
+    this.editTodoHandler = this.editTodoHandler.bind(this);
+  }
+
+  editTodoHandler = (indx) => {
+    document.querySelector(`#todo-edit_form-${indx}`).classList.remove(`${style.Disappear}`);
+    // this.setState((prevState) => ({ ...prevState, updateTodoToggle: true }));
   }
 
   render() {
     const { todoItems } = this.state;
     return (
       <ul>
-        {todoItems.map((each) => (
-          <li key={each.id} className={style.List}>
-            <div className={style.TodoListWrapper}>
+        {todoItems.map((each, index) => (
+          <li key={each.id} id={`todo-list-${index}`} className={style.List}>
+            <div className={style.TodoListWrapper} id={`todo-wrapper-${index}`}>
               <input type="checkbox" className={style.IsCompleted} aria-label={`mark your task as ${this.checked ? 'uncompleted' : 'completed'}`} />
-              <p className={style.Todos}>{each.description}</p>
+              <p
+                className={style.Todos}
+                onDoubleClick={() => { this.editTodoHandler(index); }}
+              >
+                {each.description}
+              </p>
               <button type="button" className={style.DeleteTodo}>
                 <IoMdTrash />
               </button>
             </div>
-            <input type="text" aria-label="update todo" className={`${style.UpdateFormInput} ${style.Disappear}`} />
+            <form className={`${style.TodoEditForm} ${style.Disappear}`} id={`todo-edit_form-${index}`}>
+              <input type="text" aria-label="update todo" className={`${style.UpdateFormInput}`} />
+            </form>
           </li>
         ))}
       </ul>
