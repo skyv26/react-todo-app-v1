4c8b6ff3d605f7952e8d4f6e7083837e365fb2b0
skyv26 <dev.aakashv@gmail.com>
2022-09-15 18:26:21 +0530

    Used `cb-components`

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import Header from './components/Header/Header';
 import TodoInput from './components/TodoInput/TodoInput';
 import TodoList from './components/TodoList/TodoList';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <TodoInput />
-      <TodoList />
-    </>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <>
+        <Header />
+        <TodoInput />
+        <TodoList />
+      </>
+    );
+  }
 }
 
 export default App;
