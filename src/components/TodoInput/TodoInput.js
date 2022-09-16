7083c1c411e80c012f0395945527f48fb5d51406
skyv26 <dev.aakashv@gmail.com>
2022-09-16 14:34:18 +0530

    Add `styles` to `[TodoInput]` components

--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -51,7 +51,7 @@ class TodoInput extends React.Component {
   render() {
     const { todoItems } = this.state;
     return (
-      <>
+      <div className={style.TodoInteractionWrapper}>
         <form className={style.Form} onSubmit={(e) => { this.createTodoHandler(e); }}>
           <input type="text" className={style.Text} placeholder="Add todo..." />
           <button type="submit" className={style.Submit}>
@@ -59,7 +59,7 @@ class TodoInput extends React.Component {
           </button>
         </form>
         <TodoList items={todoItems} />
-      </>
+      </div>
     );
   }
 }
