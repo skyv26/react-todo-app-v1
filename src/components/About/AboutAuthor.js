bbbeecdcd36f1b22985aa4bad35f5cac3a60b3cd
skyv26 <dev.aakashv@gmail.com>
2022-09-17 18:05:14 +0530

    Add [`Aboutt`] component

--- src/components/About/AboutAuthor.js
+++ src/components/About/AboutAuthor.js
@@ -1,11 +1,19 @@
 import React, { PureComponent } from 'react';
+import style from './About.module.css';
 
 class AboutAuthor extends PureComponent {
   render() {
     return (
-      <section className="section">
-        <h1 className="section-heading">About the Author</h1>
-        <p className="desc">This app was developed by Ibas Majid, a self taught web developer and a technical writer. He is opened to freelance Gig. So go ahead and connect with Aakash Verma on Twitter @vrma_aakash.</p>
+      <section className={style.Section}>
+        <h1 className={style.SectionHeading}>About the Author</h1>
+        <p className={style.SectionDesc}>
+          This app was developed by Aakash Verma, a self
+          taught web developer and a technical writer.
+          He is opened to freelance Gig. So go
+          ahead and connect with
+          <b> Aakash </b>
+          on Twitter @vrma_aakash.
+        </p>
       </section>
     );
   }
