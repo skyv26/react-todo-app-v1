70568ed7f872b0bc31a4e2477ba008e869fe4a37
skyv26 <dev.aakashv@gmail.com>
2022-09-17 11:31:31 +0530

    Add `Route` in application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import TodoInput from './components/TodoInput/TodoInput';
+import AboutApp from './components/About/AboutApp';
+import AboutAuthor from './components/About/AboutAuthor';
 
 class App extends React.PureComponent {
   render() {
     return (
       <>
+        <Route path="/about-app">
+          <AboutApp />
+        </Route>
+        <Route path="/about-author">
+          <AboutAuthor />
+        </Route>
         <Header />
         <TodoInput />
       </>
